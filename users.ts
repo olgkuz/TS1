35d73f4feb02a0fbc670c7a00ff937c688de18aa
olgKuz <kuznecovao@list.ru>
2025-01-23 21:24:51 +0300

    update users.ts

---
+++ users.ts
@@ -0,0 +1,20 @@
+export interface IUseGender {
+  
+  userid: string,
+  name: string,
+  gender:string
+}
+
+export const usersArray: IUseGender[] = [
+{
+  userid: '127e4567-e89b-12d3-a458-426614174000',
+
+  name: 'John',
+  gender: 'man'
+},
+{
+  userid: '127e4567-e89a-12f3-a458-327395154000',
+  name: 'Anna',
+  gender: 'woman'
+}
+]
